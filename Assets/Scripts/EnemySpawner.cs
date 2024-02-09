47d926d96717e0c6d962eccc90b96017017222d1
Ender Watts <103525727+ewatgar@users.noreply.github.com>
2024-02-09 22:26:29 +0100

    spawn animation

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -8,6 +8,7 @@ public class EnemySpawner : MonoBehaviour
     public GameObject playerPrefab;
     [Range(4,300)] public float delaySpawnSeconds = 10 ;
     public int maxEnemies = 10;
+    public int spawnAnimationSeconds = 3;
     private GameObject playerEyes;
     private int numEnemies = 0; 
 
@@ -39,9 +40,37 @@ public class EnemySpawner : MonoBehaviour
         yield return null;
     }
 
+    private IEnumerator SpawnEnemyV2(){
+        playerEyes = playerPrefab.GetComponentInChildren<Camera>().gameObject;
+
+        float range = Random.Range(0, 360);
+        Quaternion randRotation = Quaternion.Euler(new Vector3(0, range, 0));
+        Vector3 underFloor = transform.position;
+        underFloor.y = -1; //para que aparezca debajo del suelo
+        GameObject enemy = Instantiate(enemyPrefab, underFloor, randRotation);
+        enemy.GetComponentInChildren<RotadorCabeza>().target = playerEyes;
+        enemy.GetComponentInChildren<EnemigoIA>().estado = EnemigoIA.EstadoEnemigo.Parado;
+        numEnemies++;
+
+        float currentSeconds = 0;
+
+        while (currentSeconds < spawnAnimationSeconds){
+            currentSeconds += Time.deltaTime;
+            enemy.transform.position = Vector3.Lerp(underFloor, transform.position, currentSeconds / spawnAnimationSeconds);
+            yield return null;
+        }
+
+        yield return new WaitForSeconds(1);
+
+        enemy.GetComponentInChildren<EnemigoIA>().estado = EnemigoIA.EstadoEnemigo.Andando;
+        
+        yield return null;
+
+    }
+
     private IEnumerator SpawnEnemies(){
         while(numEnemies < maxEnemies){
-            StartCoroutine(SpawnEnemy());
+            StartCoroutine(SpawnEnemyV2());
             yield return new WaitForSeconds(delaySpawnSeconds);
         }
     }
